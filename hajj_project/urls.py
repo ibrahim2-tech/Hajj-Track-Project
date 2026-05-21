5a68af3d8894c757c2834cd5aa5576409673601e
ibrahim2-tech <ibrahim2362367@gmail.com>
2026-05-21 14:39:00 +0300

    Update urls.py

--- hajj_project/urls.py
+++ hajj_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # This forwards all empty/app paths to your core application!
+    path('', include('core.urls')), 
 ]
